b74733dd97e029f2752ac760c78bcddf8c61be34
qxan <mehmet.enfa@gmail.com>
2023-03-31 23:29:13 +0300

    todo list completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  tasks: Task[] = [
+    { title: 'Task 1', completed: false },
+    { title: 'Task 2', completed: true },
+    { title: 'Task 3', completed: false },
+  ];
+  newTaskTitle = '';
+
+  addTask(): void {
+    if (this.newTaskTitle) {
+      const newTask: Task = { title: this.newTaskTitle, completed: false };
+      this.tasks.push(newTask);
+      this.newTaskTitle = '';
+    }
+  }
+
+  deleteTask(task: Task): void {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  updateTask(task: Task): void {
+    task.completed = !task.completed;
+  }
 }
