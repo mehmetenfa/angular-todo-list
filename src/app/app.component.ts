568e4c4004ba12b1f6cb35ee5f5dad6fad9b6bb0
qxan <mehmet.enfa@gmail.com>
2023-04-01 00:22:16 +0300

    replaced with custom css sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Task } from './task.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   tasks: Task[] = [
